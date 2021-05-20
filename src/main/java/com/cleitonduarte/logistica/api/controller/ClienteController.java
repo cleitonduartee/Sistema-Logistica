27074736a19e34d7ccad308bd742a81d650fa828
cleiton <djcleitonduarte@hotmail.com>
2021-05-20 15:51:24 -0400

    EndPoint '/clientes/{clienteId}' no Metodo Get Criado

--- src/main/java/com/cleitonduarte/logistica/api/controller/ClienteController.java
+++ src/main/java/com/cleitonduarte/logistica/api/controller/ClienteController.java
@@ -3,7 +3,9 @@ package com.cleitonduarte.logistica.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +22,12 @@ public class ClienteController {
 	@GetMapping
 	public List<Cliente> listar(){
 		return ClienteRepository.findAll();
+	}
+	@GetMapping(value = "/{clienteId}")
+	public ResponseEntity<Cliente> obter(@PathVariable Long clienteId){
+		return ClienteRepository.findById(clienteId)
+				.map(cliente -> ResponseEntity.ok(cliente))
+				.orElse(ResponseEntity.notFound().build());				
+		
 	}
 }
