c62f948fef3271767dda47ddaa53ef9875c71395
cleiton <djcleitonduarte@hotmail.com>
2021-05-20 16:05:54 -0400

    EndPoint '/clientes' no Metodo POST criado

--- src/main/java/com/cleitonduarte/logistica/api/controller/ClienteController.java
+++ src/main/java/com/cleitonduarte/logistica/api/controller/ClienteController.java
@@ -3,10 +3,14 @@ package com.cleitonduarte.logistica.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cleitonduarte.logistica.domain.model.Cliente;
@@ -27,7 +31,12 @@ public class ClienteController {
 	public ResponseEntity<Cliente> obter(@PathVariable Long clienteId){
 		return ClienteRepository.findById(clienteId)
 				.map(cliente -> ResponseEntity.ok(cliente))
-				.orElse(ResponseEntity.notFound().build());				
+				.orElse(ResponseEntity.notFound().build());
+	}
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente cadastrar(@RequestBody Cliente newCliente) {
+		return ClienteRepository.save(newCliente);
 		
 	}
 }
